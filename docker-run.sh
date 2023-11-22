a0fe591134a8ef2157c459dc80450eeb00f39e5e
Alexander Entinger <consulting@lxrobotics.com>
2023-11-22 13:31:07 +0100

    Use macro instead.

--- docker-run.sh
+++ docker-run.sh
@@ -27,7 +27,7 @@ echo 0 > /sys/class/gpio/gpio$GPIO_CAN0_STBY/value
 
 ip link set $CAN type can bitrate $CAN_BITRATE
 ip link set $CAN up
-ifconfig can0 txqueuelen 1000
+ifconfig $CAN txqueuelen 1000
 
 sudo -u fio ifconfig $CAN
 
